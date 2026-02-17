c0c02c389925756980731e178af6dbd3ca73bb9c
Bella Vos <131202670+alleb33@users.noreply.github.com>
2026-02-17 12:39:01 -0500

    name website NextStop, display list group

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-// import ListGroup from "./components/ListGroup";
+import ListGroup from "./components/ListGroup";
 
-import Alert from "./components/Alert";
+// import Alert from "./components/Alert";
 
 function App() {
-  // let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+  let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
 
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // }
-
-  // return (
-  //   <div>
-  //     <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-  //   </div>
-  // );
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
 
   return (
-      <div>
-        <Alert>
-          Hello <span>World</span>
-        </Alert>
-      </div>
-    );
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+    </div>
+  );
+
+  // return (
+  //     <div>
+  //       <Alert>
+  //         Hello <span>World</span>
+  //       </Alert>
+  //     </div>
+  //   );
 }
 
 export default App;
