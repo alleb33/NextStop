07bb251ced78bfc3939d2937196df07e4b992c4f
Bella Vos <131202670+alleb33@users.noreply.github.com>
2026-02-17 12:09:51 -0500

    finished tutorial except exercises

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,33 +1,27 @@
-//import { Fragment } from "react";
 import { useState } from "react";
 
-function ListGroup() {
-  let items = [
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris'
-  ];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
 
-  //let selectedIndex = 0; 
-  // Hook 
+function ListGroup({items, heading, onSelectItem}: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1); 
-  // const [name, setName] = useState(''); 
-
-  // event handler 
-  //const handleClick = (event: React.MouseEvent) => console.log(event); 
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items in the list</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li 
             className = {selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
             key = {item}
-            onClick = {() => setSelectedIndex(index)}
+            onClick = {() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           
           >{item}
           </li>))}
