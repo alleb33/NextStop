19c3a68ad695fe8cc2c63d844f574413478ad489
Bella Vos <131202670+alleb33@users.noreply.github.com>
2026-04-14 19:45:36 -0400

    prepare deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// import connectDB from "./config/db";
 import app from "./server";
+import connectDb from "./config/db";
 
 const PORT = process.env.PORT || 5001;
 
 // connectDB();
+
+connectDb().then(() => {
+  console.log("Database connected");
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error("DB connection failed:", err);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
